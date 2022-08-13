87a2b5254214b80f40474678f9174e5521599351
Filip Cheliński <filipchel20@gmail.com>
2022-08-13 13:46:32 -0200

    Generate links to tag list in script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,8 @@ function titleClickHandler(event) {
       optTitleSelector = '.post-title',
       optTitleListSelector = '.titles',
       optArticleTagsSelector = '.post-tags .list',
-      optArticleAuthorSelector = '.post-author';
+      optArticleAuthorSelector = '.post-author',
+      optTagsListSelector = '.tags.list';
 
     function generateTitleLinks(customSelector = ''){
       console.log('customSelector', customSelector);
@@ -92,6 +93,10 @@ function titleClickHandler(event) {
 generateTitleLinks();
 
 function generateTags(){
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
+    console.log('allTags', allTags);
+
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
     console.log('articles', articles);
@@ -122,6 +127,14 @@ function generateTags(){
   
         /* add generated code to html variable */
         html = html + tagLinkHTML;
+
+         /* [NEW] check if this link is NOT already in allTags */
+         if(!allTags.hasOwnProperty(tag)){
+              /* [NEW] add tag to allTags object */
+              allTags[tag] = 1;
+         } else {
+           allTags[tag]++;
+         }
   
       /* END LOOP: for each tag */
       }
@@ -131,6 +144,24 @@ function generateTags(){
   
     /* END LOOP: for every article: */
     }
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      // allTagsHTML += tag  + ' (' + allTags[tag] + ') ';
+      allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag  + ' (' + allTags[tag] + ')' + '</a></li>';
+    }
+    /* [NEW] END LOOP: for each tag in allTags */
+
+    /* [NEW] add html from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+
   }
   
   generateTags();
